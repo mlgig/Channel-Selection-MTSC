3bd6adfa29c53dd6fa60ea6690bfc4c5bb502379
Bhaskar Dhariyal <bdhariyal94@gmail.com>
2021-09-15 12:37:36 +0100

    Update code files

--- SOTA/sota_wmp.sh
+++
@@ -1,5 +0,0 @@
-#Activate the environment
-source ~/mtsc/bin/activate
-#Benchmarking SOTA
-python3 -W ignore -u weasel_muse.py --path ../data --paa True --folder centroiddr --seg 0.30 0.6 0.9 >> weaseldr.txt
-#python3 ../../Generic_Scripts/combine_csv.py ./centroid weasel_muse
